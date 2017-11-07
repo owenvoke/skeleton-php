58fd6b50f0f6609de54e3961ae3b455437ea42ca
pxgamer <owzie123@gmail.com>
2017-11-07 16:37:53 +0000

    Updated to use personal values

--- prefill.php
+++ prefill.php
@@ -4,10 +4,10 @@ define('COL_HELP', 1);
 define('COL_DEFAULT', 2);
 
 $fields = [
-    'author_name' =>            ['Your name',             '',                                                ''],
-    'author_github_username' => ['Your Github username',  '<username> in https://github.com/username',       ''],
-    'author_email' =>           ['Your email address',    '',                                                ''],
-    'author_twitter' =>         ['Your twitter username', '',                                                '@{author_github_username}'],
+    'author_name' =>            ['Your name',             '',                                                'pxgamer'],
+    'author_github_username' => ['Your Github username',  '<username> in https://github.com/username',       'pxgamer'],
+    'author_email' =>           ['Your email address',    '',                                                'owzie123@gmail.com'],
+    'author_twitter' =>         ['Your twitter username', '',                                                '@pxgamer112'],
     'author_website' =>         ['Your website',          '',                                                'https://github.com/{author_github_username}'],
 
     'package_vendor' =>         ['Package vendor',        '<vendor> in https://github.com/vendor/package',   '{author_github_username}'],
@@ -20,19 +20,38 @@ $fields = [
 $values = [];
 
 $replacements = [
-    ':vendor\\\\:package_name\\\\' => function () use(&$values) { return str_replace('\\', '\\\\', $values['psr4_namespace']) . '\\\\'; },
-    ':author_name'                 => function () use(&$values) { return $values['author_name']; },
-    ':author_username'             => function () use(&$values) { return $values['author_github_username']; },
-    ':author_website'              => function () use(&$values) { return $values['author_website'] ?: ('https://github.com/' . $values['author_github_username']); },
-    ':author_email'                => function () use(&$values) { return $values['author_email'] ?: ($values['author_github_username'] . '@example.com'); },
-    ':vendor'                      => function () use(&$values) { return $values['package_vendor']; },
-    ':package_name'                => function () use(&$values) { return $values['package_name']; },
-    ':package_description'         => function () use(&$values) { return $values['package_description']; },
-    'League\\Skeleton'             => function () use(&$values) { return $values['psr4_namespace']; },
+    ':vendor\\\\:package_name\\\\' => function () use (&$values) {
+        return str_replace('\\', '\\\\', $values['psr4_namespace']) . '\\\\';
+    },
+    ':author_name'                 => function () use (&$values) {
+        return $values['author_name'];
+    },
+    ':author_username'             => function () use (&$values) {
+        return $values['author_github_username'];
+    },
+    ':author_website'              => function () use (&$values) {
+        return $values['author_website'] ?: ('https://github.com/' . $values['author_github_username']);
+    },
+    ':author_email'                => function () use (&$values) {
+        return $values['author_email'] ?: ($values['author_github_username'] . '@example.com');
+    },
+    ':vendor'                      => function () use (&$values) {
+        return $values['package_vendor'];
+    },
+    ':package_name'                => function () use (&$values) {
+        return $values['package_name'];
+    },
+    ':package_description'         => function () use (&$values) {
+        return $values['package_description'];
+    },
+    'League\\Skeleton'             => function () use (&$values) {
+        return $values['psr4_namespace'];
+    },
 ];
 
-function read_from_console ($prompt) {
-    if ( function_exists('readline') ) {
+function read_from_console($prompt)
+{
+    if (function_exists('readline')) {
         $line = trim(readline($prompt));
         if (!empty($line)) {
             readline_add_history($line);
