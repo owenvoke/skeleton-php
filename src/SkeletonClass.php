f04ccd4b9ccbda77fa30af9dd495a37b9182f9f5
pxgamer <owzie123@gmail.com>
2019-01-21 08:56:20 +0000

    Remove redundant PHPDoc comments

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -6,21 +6,11 @@ namespace pxgamer\Skeleton;
 
 class SkeletonClass
 {
-    /**
-     * Create a new Skeleton Instance
-     */
     public function __construct()
     {
         // constructor body
     }
 
-    /**
-     * Friendly welcome
-     *
-     * @param string $phrase Phrase to return
-     *
-     * @return string Returns the phrase passed in
-     */
     public function echoPhrase(string $phrase): string
     {
         return $phrase;
